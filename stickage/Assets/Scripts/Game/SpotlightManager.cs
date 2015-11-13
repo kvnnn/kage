1f77bd614562608588f06f9e41ab38123edc2879
Kevin Saito <yum02.skullcandy@gmail.com>
2015-11-14 01:22:10 +0900

    get touch position

--- stickage/Assets/Scripts/Game/SpotlightManager.cs
+++ stickage/Assets/Scripts/Game/SpotlightManager.cs
@@ -9,6 +9,37 @@ public class SpotlightManager : GameMonoBehaviour
 
 	public void Init()
 	{
-
+		DeactivateMainLight();
 	}
+
+#region MainLight
+	private void ActivateMainLight()
+	{
+		mainLightTransform.gameObject.SetActive(true);
+	}
+
+	private void DeactivateMainLight()
+	{
+		mainLightTransform.gameObject.SetActive(false);
+	}
+
+	public void StartLightCoroutine()
+	{
+		StartCoroutine(LightCoroutine());
+	}
+
+	private IEnumerator LightCoroutine()
+	{
+		// TODO : stop when game finish
+		while(true)
+		{
+			if (IsTouch())
+			{
+				UnityEngine.Debug.LogError(GetTouchPosition());
+			}
+
+			yield return null;
+		}
+	}
+#endregion
 }
